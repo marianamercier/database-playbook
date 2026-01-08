93a2954758caef2799ba47228d31724c425bc145
Mariana Mercier <49447307+marianamercier@users.noreply.github.com>
2026-01-07 22:47:27 -0300

    Delete POSTGRESQL/Active Sessions and Running Queries.sql

--- POSTGRESQL/Active Sessions and Running Queries.sql
+++
@@ -1,7 +0,0 @@
-SELECT pid, usename, application_name, client_addr,
-       state, wait_event_type, wait_event,
-       now() - query_start AS runtime,
-       query
-FROM pg_stat_activity
-WHERE state <> 'idle'
-ORDER BY runtime DESC;
